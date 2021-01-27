eaf8b252742a8f65e9e6d44588e3a074d5b0494b
swapnilkusumwal <43251574+swapnilkusumwal@users.noreply.github.com>
2021-01-28 03:09:24 +0530

    server side rendering

---
+++ client/build/precache-manifest.0be562b017c2e99a9eff24cfa326c323.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "38a3cd2da3f268983475",
+    "url": "/static/css/main.89e9b3d9.chunk.css"
+  },
+  {
+    "revision": "38a3cd2da3f268983475",
+    "url": "/static/js/main.b16b49b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4bf7003cef44ef92d7a",
+    "url": "/static/css/2.f3d606ad.chunk.css"
+  },
+  {
+    "revision": "b4bf7003cef44ef92d7a",
+    "url": "/static/js/2.cef35a6d.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8034cffc80139115e006a8b12b36a095",
+    "url": "/index.html"
+  }
+];
